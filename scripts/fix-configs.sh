38d56819caa62cbbf3f6d581ceaf6122338575e2
Stanislav Titenko <resettik@gmail.com>
2018-07-28 12:54:07 +0200

    fix gtk theme name

--- scripts/fix-configs.sh
+++ scripts/fix-configs.sh
@@ -9,7 +9,7 @@ sudo sed -i s/#EDITFILES=1/EDITFILES=0/g /etc/yaourtrc
 sudo sed -i s/#BUILD_NOCONFIRM=0/BUILD_NOCONFIRM=0/g /etc/yaourtrc
 sudo sed -i s/PKGEXT=\'.pkg.tar.xz\'/PKGEXT=\'.pkg.tar\'/g /etc/makepkg.conf
 sudo sed -i s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-greeter/g /etc/lightdm/lightdm.conf
-sudo sed -i s/#theme-name=/theme-name=Numix-ArchBlue/g /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i s/#theme-name=/theme-name=Numix-Archblue/g /etc/lightdm/lightdm-gtk-greeter.conf
 sudo sed -i s/#icon-theme-name=/icon-theme-name=Numix-Circle/g /etc/lightdm/lightdm-gtk-greeter.conf
 
 sudo sed -i -E s/^TMPDIR.*$//g /etc/yaourtrc
