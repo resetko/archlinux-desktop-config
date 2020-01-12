790640bee81726aa8bddac05823e17e8fc0cf8a6
Stanislav Titenko <resettik@gmail.com>
2020-01-12 16:14:43 +0100

    add reflector

--- scripts/install-prerequirements.sh
+++ scripts/install-prerequirements.sh
@@ -2,4 +2,5 @@ source ./scripts/constants.sh
 
 echo "Installing initially required packages"
 
-sudo pacman -Sy --needed --noconfirm base-devel wget
+sudo pacman -Sy --needed --noconfirm base-devel wget reflector
+reflector --verbose --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
